6e57471cd279969b68f7e883a48fc3ba30e753e0
Ilhana <ilhana.dzeko@zendev.se>
2025-04-22 23:12:42 +0200

    Implement ActimeContext

---
+++ Actime/Actime.Services/Database/ActimeContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Actime.Services.Database
+{
+    public class ActimeContext : DbContext
+    {
+        public ActimeContext(DbContextOptions<ActimeContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>().HasQueryFilter(e => !e.IsDeleted);
+            modelBuilder.Entity<Membership>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Notification>().HasQueryFilter(n => !n.IsDeleted);
+            modelBuilder.Entity<Organization>().HasQueryFilter(o => !o.IsDeleted);
+            modelBuilder.Entity<Participation>().HasQueryFilter(p => !p.IsDeleted);
+            modelBuilder.Entity<PaymentMethod>().HasQueryFilter(pm => !pm.IsDeleted);
+            modelBuilder.Entity<Report>().HasQueryFilter(r => !r.IsDeleted);
+            modelBuilder.Entity<User>().HasQueryFilter(u => !u.IsDeleted);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ActivityType> ActivityTypes { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<AttendanceStatus> AttendanceStatuses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventStatus> EventStatuses { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<MembershipStatus> MembershipStatuses { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Participation> Participations { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<PaymentStatus> PaymentStatuses { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<ReportType> ReportTypes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
