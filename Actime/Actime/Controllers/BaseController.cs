c3edb8ebe541632d37fcd13f457f45427e440ce4
Ilhana <ilhana.dzeko@zendev.se>
2025-10-02 21:42:07 +0200

    Implement base controllers

---
+++ Actime/Actime/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Actime.Model;
+using Actime.Model.SearchObjects;
+using Actime.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actime.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class BaseController<T, TSearch> : ControllerBase where T : class where TSearch : BaseSearchObject, new()
+    {
+        protected readonly IService<T, TSearch> _service;
+
+        public BaseController(IService<T, TSearch> service)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        [HttpGet]
+        public virtual async Task<PagedResult<T>> Get([FromQuery] TSearch? search = null) => await _service.GetAsync(search ?? new TSearch());
+
+        [HttpGet("{id}")]
+        public virtual async Task<T?> GetById(int id) => await _service.GetByIdAsync(id);
+    }
+}
---
+++ Actime/Actime/Controllers/BaseCrudController.cs
@@ -0,0 +1,29 @@
+﻿using Actime.Model.SearchObjects;
+using Actime.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actime.Controllers
+{
+    public class BaseCrudController<T, TSearch, TInsert, TUpdate> : BaseController<T, TSearch>
+        where T : class
+        where TSearch : BaseSearchObject, new()
+        where TInsert : class
+        where TUpdate : class
+    {
+        protected readonly ICrudService<T, TSearch, TInsert, TUpdate> _crudService;
+
+        public BaseCrudController(ICrudService<T, TSearch, TInsert, TUpdate> crudService) : base(crudService)
+        {
+            _crudService = crudService ?? throw new ArgumentNullException(nameof(crudService));
+        }
+
+        [HttpPost]
+        public virtual async Task<T> Create([FromBody] TInsert request) => await _crudService.CreateAsync(request);
+
+        [HttpPut("{id}")]
+        public virtual async Task<T?> Update(int id, [FromBody] TUpdate request) => await _crudService.UpdateAsync(id, request);
+
+        [HttpDelete("{id}")]
+        public virtual async Task<bool> Delete(int id) => await _crudService.DeleteAsync(id);
+    }
+}
